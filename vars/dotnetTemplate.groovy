dfa073fddfabc906ea532e944bb9f0a2ed066e84
eDyablo <eyablonsky@gmail.com>
2018-07-23 16:06:09 +0300

    Update dotnetTemplate.groovy

--- vars/dotnetTemplate.groovy
+++ vars/dotnetTemplate.groovy
@@ -1,2 +1,15 @@
-def call(body) {
+def call(String podLabel, body) {
+  podTemplate(
+    cloud: 'kubernetes-dev',
+    namespace: 'jenkins',
+    label: podLabel,
+    containers: [
+        containerTemplate(
+            name: 'dotnet',
+            image: 'microsoft/dotnet',
+            ttyEnabled: true,
+            command: 'cat')
+    ]) {
+    body()
+  }
 }
